e3dd6928e134eb7eac9545c7fb5b78416dcd96e1
viet <viet.le.worker@gmail.com>
2018-10-17 21:58:54 +0700

    a

--- DatingApp/DatingApp-SPA/src/app/app.module.ts
+++ DatingApp/DatingApp-SPA/src/app/app.module.ts
@@ -1,29 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ValueComponent } from './value/value.component';
+
 import {HttpClientModule} from '@angular/common/http';
 import { NavComponent } from './nav/nav.component';
-import { AuthService } from './_services/auth.service';
+import {FormsModule} from '@angular/forms';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ValueComponent,
-        NavComponent
-    ],
-    imports: [
-        BrowserModule,
-        HttpClientModule,
-        FormsModule
-    ],
-    providers: [
-        AuthService
-    ],
-    bootstrap: [
-        AppComponent
-    ]
+   declarations: [
+      AppComponent,
+      NavComponent,
+      RegisterComponent,
+      HomeComponent
+   ],
+   imports: [
+      BrowserModule,
+      HttpClientModule,
+      FormsModule
+   ],
+   providers: [],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule { }
